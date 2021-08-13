84006c9c79d200dfc1aca9676b6b65e19e5fda0a
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2021-08-13 18:32:25 +0700

    Update

--- src/ProcessBar.js
+++ src/ProcessBar.js
@@ -9,14 +9,14 @@ export default class ProcessBar extends Container {
         this.score = 0;
 
         this.roundBox = new Graphics();
-        this.roundBox.lineStyle(5, 0xFF00FF, 1);
+        this.roundBox.lineStyle(3, 0xFF00FF, 1);
         this.roundBox.beginFill(0x1C1C1C);
         this.roundBox.drawRect(0.2 * this.blockSize, 0.2 * this.blockSize, 5 * this.blockSize, this.blockSize / 3, 10)
         this.roundBox.endFill();
         this.addChild(this.roundBox);
 
         this.processBar = new Graphics();
-        this.processBar.lineStyle(5, 0xFF00FF, 1);
+        this.processBar.lineStyle(3, 0xFF00FF, 1);
         this.processBar.beginFill(0x00FFCC);
         this.processBar.drawRect(0.2 * this.blockSize, 0.2 * this.blockSize, (this.score / this.targetScore) * 5 * this.blockSize, this.blockSize / 3, 10)
         this.processBar.endFill();
